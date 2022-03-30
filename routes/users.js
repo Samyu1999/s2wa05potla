99ba1840ceaf26dfc779f68b242239d19d68733c
Samyu1999 <samyukthap09@gmail.com>
2022-03-30 18:49:19 -0500

    user file updated

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var times=0;
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   times++;
-  res.send('respond with a resource');
+  res.send(`User accesses are: ${times}`);
 });
 
 module.exports = router;
